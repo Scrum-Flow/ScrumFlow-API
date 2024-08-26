3c073605c4162ad4614f65995c375b9a1d978002
Erik <efvergani@hotmail.com>
2024-08-25 21:54:49 -0300

    Initial Commit

---
+++ src/main/java/com/scrumflow/api/ScrumFlowApplication.java
@@ -0,0 +1,14 @@
+package com.scrumflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class ScrumFlowApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumFlowApplication.class, args);
+    }
+}
---
+++ src/test/java/com/scrumflow/api/ScrumFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrumflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumFlowApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
