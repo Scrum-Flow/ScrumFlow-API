0d942ecc0b58bbe4f3d1a003d977a9f9cb73ebea
Erik <efvergani@hotmail.com>
2024-09-06 19:31:59 -0300

    TASK-47: Criada entidade e repositorio para o time

---
+++ src/main/java/com/scrumflow/domain/model/Team.java
@@ -0,0 +1,26 @@
+package com.scrumflow.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "teams")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+}
