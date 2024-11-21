eb6a78b4c5321a002bb1caa0dde5def5e1e8d281
Erik <efvergani@hotmail.com>
2024-11-20 23:05:58 -0300

    FIX: Removida ref da database no schema

--- src/main/resources/db/migration/1.0.0/V1.0.0_0016__insert_into_role.sql
+++ src/main/resources/db/migration/1.0.0/V1.0.0_0016__insert_into_role.sql
@@ -1,3 +1,3 @@
-INSERT INTO scrumflow.`role`
+INSERT INTO `role`
 (id, name, description, created_at, updated_at)
 VALUES(5, 'TEAM_MEMBER', 'Função responsável por executar atividades dentro dos projetos', NOW(), NOW());
