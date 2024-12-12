b6004ac1efc347c41759b8c9ae93be85f48004a7
Gabriel Schmogel <gabriel.schmogel@dbserver.com.br>
2024-12-12 12:37:53 -0300

    fix: aidiconado seletor de notificações ao dto do usuário

--- src/main/java/com/scrumflow/application/dto/response/UserResponseDTO.java
+++ src/main/java/com/scrumflow/application/dto/response/UserResponseDTO.java
@@ -3,4 +3,9 @@ package com.scrumflow.application.dto.response;
 import java.util.List;
 
 public record UserResponseDTO(
-        Long id, String name, String email, boolean active, List<RoleResponseDTO> roles) {}
+        Long id,
+        String name,
+        String email,
+        boolean active,
+        List<RoleResponseDTO> roles,
+        Boolean sendNotifications) {}
